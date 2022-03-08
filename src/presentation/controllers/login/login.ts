4313162226c045569144b36745476921b8024ea0
Guilherme Carvalho <guisegtrab@gmail.com>
2022-03-08 11:43:10 -0300

    feat: certifique-se de que o LoginController retorne 400 se um email inválido for fornecido

---
+++ src/presentation/controllers/login/login.ts
@@ -0,0 +1,25 @@
+import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+import { badRequest } from '../../helpers/http-helper'
+import { MissingParamError, InvalidParamError } from '../../errors'
+import { EmailValidator } from '../signup/signup-controller-protocols'
+
+export class LoginController implements Controller {
+  private readonly emailValidator: EmailValidator
+
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    if (!httpRequest.body.email) {
+      return new Promise(resolve => resolve(badRequest(new MissingParamError('email'))))
+    }
+    if (!httpRequest.body.password) {
+      return new Promise(resolve => resolve(badRequest(new MissingParamError('password'))))
+    }
+    const isValid = this.emailValidator.isValid(httpRequest.body.email)
+    if (!isValid) {
+      return new Promise(resolve => resolve(badRequest(new InvalidParamError('email'))))
+    }
+  }
+}
